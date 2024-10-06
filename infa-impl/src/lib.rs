ee50883ac716314d7fb58686e5a1315f9533f72c
Googlefan <google@neody.dev>
2024-10-06 20:37:01 +0000

    feat(infa-impl): add modules for various numeric types

--- infa-impl/src/lib.rs
+++ infa-impl/src/lib.rs
@@ -1,3 +1,20 @@
+mod int64;
+pub use int64::*;
+mod int16;
+pub use int16::*;
+mod int32;
+pub use int32::*;
+mod int8;
+pub use int8::*;
+mod float16;
+pub use float16::*;
+mod float32;
+pub use float32::*;
+mod float64;
+pub use float64::*;
+mod bfloat16;
+pub use bfloat16::*;
+
 use std::ops::{Add, Sub};
 
 pub trait TensorOps<'a, T: 'a, E>:
